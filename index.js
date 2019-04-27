51076041135c2f96df55a04e26142ec5169dda60
Nicolas Marini <nmarini44@gmail.com>
2019-04-27 21:54:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function lowerCaseDrivers(array) {
 
 function nameToAttributes(array) {
   return array.map(function(fullName) {
-    return Object.assign({}, fullName, {
+    return Object.assign({}, {
       firstName: fullName.split(' ').slice(0, -1).join(" "),
        lastName: fullName.split(' ').slice(-1).join(" ")
      })
