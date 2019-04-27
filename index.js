cb01fc432944e6926011bb273de88f089918e647
Nicolas Marini <nmarini44@gmail.com>
2019-04-27 21:47:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function nameToAttributes(array) {
   return array.map(function(fullName) {
     let first = fullName.split(' ').slice(0, -1).join()
     let last = fullName.split(' ').slice(-1).join()
-    return Object.assign({}, driver, {firstName: first, lastName: last})
+    return Object.assign({}, fullName, {firstName: first, lastName: last})
   })
 }
