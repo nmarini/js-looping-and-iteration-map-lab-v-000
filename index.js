13b2275e58a8b6027da8f6ec3c3959bcac4469bb
Nicolas Marini <nmarini44@gmail.com>
2019-04-27 21:47:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function nameToAttributes(array) {
   return array.map(function(fullName) {
     let first = fullName.split(' ').slice(0, -1).join()
     let last = fullName.split(' ').slice(-1).join()
+    debugger
     return Object.assign({}, fullName, {firstName: first, lastName: last})
   })
 }
